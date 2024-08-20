7faf43e32acebdc0faa898d87a93adce76b60c37
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-19 20:05:41 -0400

    feat: model of the entities added

---
+++ LibrarySystem/src/Model/Models/Fine.cs
@@ -0,0 +1,15 @@
+﻿namespace LibrarySystem;
+
+public class Fine : EntityBase
+{
+    public required Loan Loan { get; set; }
+    public required double FineAmount { get; set; }
+    public bool WasPayed { get; set; } = false;
+
+    public override string ToString()
+    {
+        return "Fine: " + FineAmount + "$ | " 
+            + (WasPayed ? "paid" : "active") 
+            + "\n\t" + Loan;
+    }
+}

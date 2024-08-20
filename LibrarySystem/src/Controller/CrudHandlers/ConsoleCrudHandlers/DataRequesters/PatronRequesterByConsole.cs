5fe7bce246467678a874ce3b1560e9815c439376
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-19 21:33:31 -0400

    feat: added crud handlers; helpers for the execution of the controllers

---
+++ LibrarySystem/src/Controller/CrudHandlers/ConsoleCrudHandlers/DataRequesters/PatronRequesterByConsole.cs
@@ -0,0 +1,70 @@
+﻿namespace LibrarySystem;
+
+public class PatronRequesterByConsole : IEntityRequester<Patron>
+{
+
+    private AbstractMessageRenderer _renderer;
+
+    private IReceiver<string> _receiver;
+
+    public PatronRequesterByConsole(IReceiver<string> receiver, AbstractMessageRenderer renderer)
+    {
+        _receiver = receiver;
+        _renderer = renderer;
+    }
+
+    public Patron? AskForEntity()
+    {
+        Patron? requestedPatron = null;
+        try
+        {
+            var patronToValidate = ReceivePatronByConsole();
+            PatronValidator.ValidatePatron(patronToValidate);
+            requestedPatron = patronToValidate;
+        }
+        catch (BookException ex)
+        {
+            _renderer.RenderErrorMessage(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            _renderer.RenderErrorMessage(ex.Message);
+        }
+
+        return requestedPatron;
+    }
+
+    private Patron ReceivePatronByConsole()
+    {
+        _renderer.RenderSimpleMessage("Enter the name: ");
+        var name = _receiver.ReceiveInput();
+        _renderer.RenderSimpleMessage("Enter the contact details: ");
+        var contactDetails = _receiver.ReceiveInput();
+        var membershipNumber = GetMembershipNumberAsNumber();
+
+        var patronReceived = new Patron
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            MembershipNumber = membershipNumber,
+            ContactDetails = contactDetails
+        };
+        return patronReceived;
+    }
+
+    private int GetMembershipNumberAsNumber()
+    {
+        int membershipNumber = -1;
+        do
+        {
+            _renderer.RenderSimpleMessage("Enter the membership number: ");
+            var numberAsText = _receiver.ReceiveInput();
+            if (int.TryParse(numberAsText, out int number))
+            {
+                membershipNumber = number;
+            }
+        } while (membershipNumber == -1);
+
+        return membershipNumber;
+    }
+}
---
+++ LibrarySystem/src/Controller/CrudHandlers/ConsoleCrudHandlers/EntityUpdaterByConsole.cs
@@ -0,0 +1,50 @@
+﻿namespace LibrarySystem;
+
+public class EntityUpdaterByConsole<T> : IEntityUpdater<T, string> where T : EntityBase
+{
+    private IRepository<T> _repository;
+    private IEntityRequester<T> _entityRequester;
+    private AbstractMessageRenderer _renderer;
+    private EntitySelectorByConsole<T> _entitySelector;
+
+    public EntityUpdaterByConsole(IRepository<T> repository, IEntityRequester<T> entityRequester, AbstractMessageRenderer renderer, EntitySelectorByConsole<T> entitySelector)
+    {
+        _repository = repository;
+        _entityRequester = entityRequester;
+        _renderer = renderer;
+        _entitySelector = entitySelector;
+    }
+
+    public void TryToUpdateEntity()
+    {
+        var entities = _repository.GetAll();
+        var entity = _entitySelector.TryToSelectAtLeastOne(entities);
+        if (entity is not null)
+        {
+            var newEntityData = _entityRequester.AskForEntity();
+            if (newEntityData is not null)
+            {
+                newEntityData.Id = entity.Id;
+                var wasUpdated = _repository.Update(newEntityData);
+                RenderDeleteStatus(wasUpdated);
+            }
+        }
+
+        if (entities.Any() && entity is null)
+        {
+            _renderer.RenderErrorMessage("try again...");
+        }
+    }
+
+    private void RenderDeleteStatus(bool wasUpdated)
+    {
+        if (wasUpdated)
+        {
+            _renderer.RenderSuccessMessage("updated");
+        }
+        else
+        {
+            _renderer.RenderErrorMessage("something bad happened. Please try again.");
+        }
+    }
+}

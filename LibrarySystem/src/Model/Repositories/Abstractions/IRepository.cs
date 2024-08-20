6cf054dc9b87d9f7b2e892013977bd3335369b5b
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-19 20:06:44 -0400

    feat: repository for aggregate entities; signature of the repositories and their concrete implementations

---
+++ LibrarySystem/src/Model/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace LibrarySystem;
+
+public interface IRepository<T> where T : EntityBase
+{
+    bool Save(T item);
+    bool Update(T item);
+    bool Delete(T item);
+    T? GetById(Guid itemId);
+    List<T> GetAll();
+}
---
+++ LibrarySystem/src/Model/Repositories/Concrete/PatronRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace LibrarySystem;
+
+public class PatronRepository : BaseRepository<Patron>, IPatronRepository
+{
+    public Patron? GetByName(string name)
+    {
+        return Data.Values
+                   .FirstOrDefault(patron =>
+                   patron.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public Patron? GetByMembershipNumber(int membershipNumber)
+    {
+        return Data.Values
+                   .FirstOrDefault(patron =>
+                   patron.MembershipNumber == membershipNumber);
+    }
+}

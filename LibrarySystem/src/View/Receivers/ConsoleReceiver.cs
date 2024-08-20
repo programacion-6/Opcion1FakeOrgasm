52d01b94569d7ca6db099831af3066f3280af061
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-08-19 21:30:27 -0400

    feat: added input receivers

---
+++ LibrarySystem/src/View/Receivers/ConsoleReceiver.cs
@@ -0,0 +1,21 @@
+﻿namespace LibrarySystem;
+
+public class ConsoleReceiver : IReceiver<string>
+{
+    public string ReceiveInput()
+    {
+        var indicator = ConsoleFormatter.AsAHighlight(">> ");
+        Console.Write(indicator);
+        var inputReceived = Console.ReadLine();
+        if (inputReceived is null)
+        {
+            var invalidInputError = ConsoleFormatter.AsAnError("invalid input");
+            Console.WriteLine(invalidInputError);
+            return ReceiveInput();
+        }
+        else
+        {
+            return inputReceived;
+        }
+    }
+}

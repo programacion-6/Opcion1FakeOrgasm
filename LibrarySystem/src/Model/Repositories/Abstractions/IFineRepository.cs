28de85c122985d5fb05b3cce652464b07856b6d2
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-23 00:41:50 -0400

    refactor: add ILoanRepository attribute in GetFinesByPatron method in IFineRepository interface

--- LibrarySystem/src/Model/Repositories/Abstractions/IFineRepository.cs
+++ LibrarySystem/src/Model/Repositories/Abstractions/IFineRepository.cs
@@ -3,6 +3,6 @@
 public interface IFineRepository : IRepository<Fine>
 {
     Fine? GetByLoan(Loan loan);
-    List<Fine> GetFinesByPatron(Patron patron);
+    List<Fine> GetFinesByPatron(Patron patron, ILoanRepository loanRepository);
     List<Fine> GetActiveFines();
 }

82b0daa7d9a4b1b7ef52adf1813e458f01ee7662
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-22 22:48:44 -0400

    fix: add book and pattern repositories in the Reposter class
    Adding the repositories will help to search for the ids and then return the correct response according to the id found, to provide a report through the console according to what the user chose.

--- LibrarySystem/src/Model/Services/Reporter.cs
+++ LibrarySystem/src/Model/Services/Reporter.cs
@@ -2,36 +2,70 @@
 
 public class Reporter
 {
-    private readonly ILoanRepository loanRepository;
+    private readonly ILoanRepository _loanRepository;
 
-    public Reporter(ILoanRepository loanRepository)
+    private readonly IBookRepository _bookRepository;
+
+    private readonly IPatronRepository _patronRepository;
+
+    public Reporter(ILoanRepository loanRepository, IBookRepository bookRepository, IPatronRepository patronRepository)
     {
-        this.loanRepository = loanRepository;
+        _loanRepository = loanRepository;
+        _bookRepository = bookRepository;
+        _patronRepository = patronRepository;
     }
 
     public List<Book> GetCurrentlyBorrowedBooks()
     {
-        var currentlyLoans = loanRepository.GetCurrentlyLoans();
-        var currentlyBorrowedBooks = currentlyLoans.Select(loan => loan.Book).ToList();
-        return currentlyBorrowedBooks;
+        var currentlyLoans = _loanRepository.GetCurrentlyLoans();
+
+        var borrowedBookIds = currentlyLoans.Select(loan => loan.IdBook).ToList();
+
+        var currentlyBorrowedBooks = borrowedBookIds
+            .Select(id => _bookRepository.GetById(id))
+            .Where(book => book != null)
+            .ToList();
+
+        return currentlyBorrowedBooks!;
     }
 
     public List<Patron> GetPatternsThatBorrowedBooks()
     {
-        var currentlyLoans = loanRepository.GetCurrentlyLoans();
-        var patrons = currentlyLoans.Select(loan => loan.Patron).ToList();
+        var currentlyLoans = _loanRepository.GetCurrentlyLoans();
+
+        var patronIds = currentlyLoans
+            .Select(loan => loan.IdPatron)
+            .Distinct()
+            .ToList();
+
+        var patrons = patronIds
+            .Select(id => _patronRepository.GetById(id))
+            .Where(patron => patron != null)
+            .ToList();
+
         return patrons;
     }
 
+
     public List<Book> GetOverdueBooks()
     {
-        var overdueLoans = loanRepository.GetOverdueLoans();
-        var overdueBooks = overdueLoans.Select(loan => loan.Book).ToList();
+        var overdueLoans = _loanRepository.GetOverdueLoans();
+
+        var overdueBookIds = overdueLoans
+            .Select(loan => loan.IdBook)
+            .Distinct()
+            .ToList();
+
+        var overdueBooks = overdueBookIds
+            .Select(id => _bookRepository.GetById(id))
+            .Where(book => book != null)
+            .ToList();
+
         return overdueBooks;
     }
 
     public List<Loan> GetLoansByPatron(Patron patron)
     {
-        return loanRepository.GetLoansByPatron(patron);
+        return _loanRepository.GetLoansByPatron(patron);
     }
 }
